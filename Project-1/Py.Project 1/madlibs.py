d9935e476af88681cb7d244dc1ea2652450ec980
Muhammad-Aashib <muhammadaashib2707@gmail.com>
2025-02-24 01:44:50 +0500

    Add files via upload

---
+++ Project-1/Py.Project 1/madlibs.py
@@ -0,0 +1,7 @@
+name = input("name : ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+adj = input("Adjective: ")
+
+madlib = f"{name} created this project very {adj} and {verb1} it thoroughly, and {verb2} its design "
+print(madlib)
